2f9aa37c518f3d8a8a4fdb6b696519d2ea9907f6
ezhamkarim <52437541+ezhamkarim@users.noreply.github.com>
2025-02-25 19:04:05 +0800

    first commit

---
+++ lib/src/views/kindergarten_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KindergartenDetailsView extends StatelessWidget {
+  const KindergartenDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/src/views/kindergarten_listing.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KindergartenListing extends StatelessWidget {
+  const KindergartenListing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
